3e8d173d9cea86735fccd289f35299bc072b016f
Mucio Osorio <mucio.osorio@itson.edu.mx>
2015-01-09 19:02:51 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,8 +1,11 @@
-## Extraer archivo de datos de febrero 1 y 2 de 2007 del archivo original (Data File February 1 and 2, 2007 from original data file)
+## Extraer archivo de datos de febrero 1 y 2 de 2007 del archivo original 
+## (Data File February 1 and 2, 2007 from original data file)
 
-datos <- subset(read.csv("household_power_consumption.txt",header = TRUE,sep =";",na.strings="?"),Date == "2/2/2007"| Date=="1/2/2007")
+datos <- subset(read.csv("household_power_consumption.txt",header = TRUE,sep =";",na.strings="?"),
+                Date == "2/2/2007"| Date=="1/2/2007")
 
-## Extraer la columna Date y Time, unirlas y agregarlas al archivodatos (Extracting Date and Time column, join and add to the data file)
+## Extraer la columna Date y Time, unirlas y agregarlas al archivodatos 
+## (Extracting Date and Time column, join and add to the data file)
 
 datos$Date <- as.Date(datos$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(datos$Date), datos$Time)
